14ef24eab51203ca544c8f8624a7074fd660e5b1
Anup Angadi <52240005+Anup9148680234@users.noreply.github.com>
2023-05-04 13:24:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //your code here
+document.getElementById('letterCount').innerText =0;
 let inp = document.getElementById('evaluatedText');
 inp.addEventListener('input',()=>{
 	document.getElementById('letterCount').innerText = inp.value.length;
